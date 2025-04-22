b9bbd625b3959c0d2ecd5a6353a91d5546a2186d
BottomToTheTop <121840768+BottomToTheTop@users.noreply.github.com>
2025-04-22 11:47:45 +0100

    Complete 03_reverseString exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    reverse = [];
+    reverseWord = '';
+    for (let i = 0; i < word.length; i++) {
+        reverse.push(word[i])
+    }
+    reverse.reverse();
+    while(reverse.length > 0) {
+        reverseWord += reverse.shift()
+    }
+    return reverseWord;
 };
 
 // Do not edit below this line
