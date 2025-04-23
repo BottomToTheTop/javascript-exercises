0548706ca2281af8ea06c2baeeb192dd3bde9278
BottomToTheTop <121840768+BottomToTheTop@users.noreply.github.com>
2025-04-23 03:25:13 +0100

    Complete 04_removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr) {
+    for (let i = 1; i < arguments.length; i++) { // Goes through each argument
+        for (let x = 0; x < arr.length; x++) { // Checks each element in array for argument
+            if (arr[x] === arguments[i]) {
+                arr.splice(x, 1)
+                i--;
+            }
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
