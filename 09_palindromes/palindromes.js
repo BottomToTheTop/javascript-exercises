a938aa2918a15392e448d75176776aac33d5b042
BottomToTheTop <121840768+BottomToTheTop@users.noreply.github.com>
2025-05-16 21:31:43 +0100

    Complete 09_palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function(string) {
+    stringArray = string.split("")
+    filteredString = stringArray.filter((item) => {
+        return item.match(/[A-Za-z0-9]/i);
+    });
+    a = filteredString.reverse().join("").toLowerCase();
+    b = filteredString.reverse().join("").toLowerCase();
+    if (a === b) {
+        return true;
+    } else {
+        return false;
+    }
 };
-
 // Do not edit below this line
 module.exports = palindromes;
