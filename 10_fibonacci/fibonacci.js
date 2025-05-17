2faeb530e9ceab61fa7e6997dafc87a38ddad083
BottomToTheTop <121840768+BottomToTheTop@users.noreply.github.com>
2025-05-17 20:21:15 +0100

    Complete 10_fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,14 @@
-const fibonacci = function() {
-
-};
-
+const fibonacci = function(num) {
+    if (num < 0) {
+        return "OOPS";
+    }
+    
+    fib = [0, 1]
+    for (let i = 2; i <= num; i++) {
+        x = fib[i - 1] + fib[i - 2]
+        fib.push(x)
+    }
+    return fib[num];
+}
 // Do not edit below this line
 module.exports = fibonacci;
