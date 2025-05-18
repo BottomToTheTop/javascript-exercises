0c7bcdb0f233e9a04657d21c265302ada336c34d
BottomToTheTop <121840768+BottomToTheTop@users.noreply.github.com>
2025-05-18 18:03:38 +0100

    Complete 12_findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,14 @@
-const findTheOldest = function() {
-
-};
-
+const findTheOldest = function(arr) {
+    peopleAges = arr.map((item) => {
+        if (!item.yearOfDeath) {
+            const date = new Date()
+            return age = date.getFullYear() - item.yearOfBirth;
+        }
+        
+        return age = item.yearOfDeath - item.yearOfBirth;
+    });
+    oldestAgeIndex = peopleAges.indexOf(Math.max(...peopleAges));
+    return arr[oldestAgeIndex];
+}
 // Do not edit below this line
 module.exports = findTheOldest;
